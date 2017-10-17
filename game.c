d53e552296b34b5cb21244ff7d6d2bdc0ebf8bdb
Lewis <Lewisgarton@gmail.com>
2017-10-18 02:23:25 +1300

    ir finaly working (ish)

--- game.c
+++ game.c
@@ -27,11 +27,6 @@ void display_character (char character)
 /* Function to display a scrolling text in the matrix */
 void display_string (char* message)
 {
-    // Seting up the display settings
-    tinygl_font_set (&font3x5_1);
-    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
-    tinygl_text_dir_set (TINYGL_TEXT_DIR_ROTATE);
-    tinygl_text_speed_set (MESSAGE_RATE);
     // Sending the messasge to the display
     tinygl_text (message);
 
@@ -60,10 +55,6 @@ bool game_status (int start)
         // Clearing the display
         tinygl_clear();
     }
-    // Game is started by pulling down the navigation button
-    if (navswitch_push_event_p (NAVSWITCH_WEST)) {
-        start = 0;
-    }
 
     return start;
 }
@@ -75,15 +66,9 @@ int main (void)
     char character[3] = {'P','S','R'};
     // Scrolling screen text
     char* scroll_screen = "PRESS DOWN BUTTON";
-    char opponent_char;
-    char player_char;
-
-    int i = 1;
-
 
     bool chosen = false;
-    choice_t player = 0;
-
+    int player = 0;
 
     // Initialising systems
     system_init ();
@@ -91,19 +76,21 @@ int main (void)
     my_matrix_init();
     // Initialising inputs
     navswitch_init();
-    // Initialising ir communication driver
+
     ir_uart_init ();
 
     // Displaying the scroll screen
     display_string(scroll_screen);
 
-
+    uint8_t ch = -1;
+    bool rcv = false;
+    bool snd = false;
     while(1) {
 
         // Checing for the state of the game
         bool start = 0;
         start = game_status(start);
-
+        tinygl_update ();
 
         // Scrolling startup screen before game starts
         while (start == 0) {
@@ -115,60 +102,66 @@ int main (void)
 
         // Staring the game
         while (start == 1) {
+
             pacer_wait ();
             tinygl_update ();
-            i = 1;
-
-
-            // Checking the navswitch
             navswitch_update ();
 
-            // Scrolling right in the list
-            if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
-                if (player < 2) {
-                    player += 1;
+            // Untill player has chosen
+            if (chosen == false) {
+
+
+                // Scrolling right in the list
+                if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
+                    if (player < 2) {
+                        player += 1;
+                    }
                 }
-            }
 
-            // Scrolling left in the list
-            if (navswitch_push_event_p (NAVSWITCH_SOUTH)) {
-                if (player > 0) {
-                    player -= 1;
+                // Scrolling left in the list
+                if (navswitch_push_event_p (NAVSWITCH_SOUTH)) {
+                    if (player > 0) {
+                        player -= 1;
+                    }
                 }
-            }
 
-            // Showing the current letter in the display
-            display_character (character[player]);
+                // Showing the current letter in the display
+                display_character (character[player]);
 
-            // Checking if the player has choosen
-            if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
-                chosen = true;
-                // Player choosen chracter
-                player_char = character[player];
-                // sending the choosen chracter through IR
-                ir_uart_putc (player_char);
-                tinygl_clear();
-                i = 0;
-            }
+                if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
+                    chosen = true;
 
-            while (i == 0) {
-                // Reciving the selection of the opponent
-                if (ir_uart_read_ready_p ()) {
-                    opponent_char = ir_uart_getc ();
-                    tinygl_update();
-                    display_character (opponent_char);
-                    i = 1;
+                    tinygl_clear();
+
                 }
 
             }
+
+                if(rcv == false && chosen == true) {
+                    if(ir_uart_read_ready_p()) {
+                        ch = ir_uart_getc();
+                        display_character (ch);
+                        rcv = true;
+
+                    }
+                }
+                if(snd == false && chosen == true) {
+                    if(ir_uart_write_ready_p()) {
+                        ir_uart_putc(temp);
+                        if (ir_uart_write_finished_p()) {
+                            snd = true;
+                        }
+                    }
+                }
+
+
+
 
 
 
 
 
         }
-
-
 
     }
 
