2e0c13f92fb22c422fca07f11912945a03569c85
Wijesundara Ranasinghe Appu Gunathilake <jwr80@cs14143jm.canterbury.ac.nz>
2017-10-19 14:54:38 +1300

    Commented on the files

--- game.c
+++ game.c
@@ -1,3 +1,13 @@
+/** @file   game.c
+    @authors Janitha Gunathhilake, Lewis Garton
+    @date   19 Oct 2017
+    @brief  Main program to run the paper, scissors, rock game
+
+    initializes required programs/modules and exacutes the  paper,
+    * scissors, rock game.
+*/
+
+
 #include "system.h"
 #include "pacer.h"
 #include "navswitch.h"
@@ -63,6 +73,8 @@ bool game_status (int start)
     return start;
 }
 
+// Function to scroll through the game variable list and
+// select the game variable
 void choose_selection(state_t* game_state)
 {
     navswitch_update ();
@@ -100,37 +112,42 @@ void choose_selection(state_t* game_state)
     }
 }
 
+// Function to set the player number
+void select_player(state_t* game_state)
+{
 
-void select_player(state_t* game_state){
-	navswitch_update ();
-	if (navswitch_push_event_p (NAVSWITCH_WEST)) {
-		game_state->player = '1';
-	}
-	
-	if (navswitch_push_event_p (NAVSWITCH_EAST)) {
-		game_state->player = '2';
-	}	
-	
-	display_character (game_state->player);
-	
-	if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
-            game_state->player_selected = true;
-            tinygl_clear();
+    navswitch_update ();
+
+    if (navswitch_push_event_p (NAVSWITCH_WEST)) {
+        game_state->player = '1';
+    }
+
+    if (navswitch_push_event_p (NAVSWITCH_EAST)) {
+        game_state->player = '2';
+    }
+
+    display_character (game_state->player);
+
+    // Selects the player and clear the screen
+    if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
+        game_state->player_selected = true;
+        tinygl_clear();
     }
 }
 
 // Send message
 void snd_ir(state_t* game_state,uint8_t message)
 {
-	if(game_state->sent == false){	
-		if(ir_uart_write_ready_p()) {
-			ir_uart_putc(message);
-		}
-		
-		if(ir_uart_write_finished_p()) {
-			game_state->sent = true;
-		}
-	}
+    if(game_state->sent == false) {
+        if(ir_uart_write_ready_p()) {
+            ir_uart_putc(message);
+        }
+
+        if(ir_uart_write_finished_p()) {
+            // Check if send
+            game_state->sent = true;
+        }
+    }
 }
 
 
@@ -146,15 +163,17 @@ uint8_t rcv_ir(void)
 }
 
 
-
-void get_result(state_t* state) {
+// Function to get the results of the game 
+// and decide winner, looser or draw
+void get_result(state_t* state)
+{
     if(state->result == 0) {
         char result;
         if (state->selection == state->opponent) {
             result = 'D';
         } else if (state->selection == 'R' && state->opponent == 'S') {
             result = 'W';
-        } else if (state->selection == 'P' && state->opponent == 'R'){
+        } else if (state->selection == 'P' && state->opponent == 'R') {
             result = 'W';
         } else if (state->selection == 'S' && state->opponent == 'P') {
             result = 'W';
@@ -165,109 +184,100 @@ void get_result(state_t* state) {
         state->result_set = true;
     }
 }
-void invert_result(state_t* game_state){
-	if(game_state->result == 'D'){
-		game_state->result = 'D';
-	} else if (game_state->result == 'W') {
-		game_state->result = 'L';
-	} else {
-		game_state->result = 'W';
-	}	
-}
 
-
-// reset the game state to play again
-void reset_game(state_t* game_state){
-	game_state->selection = 'P';
-	game_state->opponent = 0;
-	game_state->selection_final = false;
-	game_state->result = 0;
-	game_state->confirm_selection = 0;
-	game_state->result_set = false;
-	game_state->sent = false;
-	game_state->game_over = false;
-	
-	
-	// clear display
-	tinygl_clear();
-	/*
-	nt player;
-    uint8_t selection;
-    uint8_t opponent;
-    bool selection_final;
-    uint8_t result;
-    int score;
-    uint8_t confirm_selection;
-    bool result_set;
-	
-	
-	
-	{0,'P',0,false,0,0,0,false};
-	*/
-}
-
-
-void get_opponent(state_t* game_state){
-        uint8_t read_choice;
-        read_choice = rcv_ir();
-        if(read_choice == 'R' || read_choice == 'S' || read_choice == 'P') {
-            game_state->opponent = read_choice;
-        }
+// Function to invert the result for the other player 
+void invert_result(state_t* game_state)
+{
+    if(game_state->result == 'D') {
+        game_state->result = 'D';
+    } else if (game_state->result == 'W') {
+        game_state->result = 'L';
+    } else {
+        game_state->result = 'W';
     }
-
-void ir_rcv_result(state_t* game_state){
-        uint8_t read_choice;
-        read_choice = rcv_ir();
-        if(read_choice == 'W' || read_choice == 'L' || read_choice == 'D') {
-            game_state->result = read_choice;
-            game_state->result_set = true;
-        }
-    }
-
-
-
-
-void display_result(state_t* game_state){
-	if(game_state->game_over == false && game_state->sent){
-		
-		if(game_state->result == 'W') {
-			display_string("WINNER");
-		} else if (game_state->result == 'L') {
-			display_string("LOOSER");
-		} else {
-			display_string("DRAW");		
-		}
-	game_state->game_over = true;	
-	}
 }
 
 
+// Function to reset the game state to play again
+void reset_game(state_t* game_state)
+{
+    game_state->selection = 'P';
+    game_state->opponent = 0;
+    game_state->selection_final = false;
+    game_state->result = 0;
+    game_state->confirm_selection = 0;
+    game_state->result_set = false;
+    game_state->sent = false;
+    game_state->game_over = false;
+
+
+    // clear display
+    tinygl_clear();
+}
+
+// Function to check if opponent selection recieved
+void get_opponent(state_t* game_state)
+{
+    uint8_t read_choice;
+    read_choice = rcv_ir();
+    if(read_choice == 'R' || read_choice == 'S' || read_choice == 'P') {
+        game_state->opponent = read_choice;
+    }
+}
+
+// Function to check if the results been recived
+void ir_rcv_result(state_t* game_state)
+{
+    uint8_t read_choice;
+    read_choice = rcv_ir();
+    if(read_choice == 'W' || read_choice == 'L' || read_choice == 'D') {
+        game_state->result = read_choice;
+        game_state->result_set = true;
+    }
+}
+
+
+
+// Function to display the results
+void display_result(state_t* game_state)
+{
+    if(game_state->game_over == false && game_state->sent) {
+
+        if(game_state->result == 'W') {
+            display_string("WINNER");
+        } else if (game_state->result == 'L') {
+            display_string("LOOSER");
+        } else {
+            display_string("DRAW");
+        }
+        game_state->game_over = true;
+    }
+}
 
 
 
 int main (void)
-{	
+{
+    // Tick counter
     uint16_t tick = 0;
-    // Arrays for selecting the game variables
+    
     // Scrolling screen text
     char* scroll_screen = "SELECT PLAYER PRESS DOWN BUTTON";
-    //bool chosen = false;
-
-    // Initialising systems
+    
+    // Initialising all systems
     system_init ();
-    // Initialising display driver
     my_matrix_init();
-    // Initialising inputs
     navswitch_init();
-
     ir_uart_init ();
 
     // Displaying the scroll screen
     display_string(scroll_screen);
 
-
+    // Initializing for game state
     state_t game_state = {'1','P',0,false,0,0,0,false, false, false, false};
+    
     while(1) {
+        
         // Checing for the state of the game
         bool start = 0;
         start = game_status(start);
@@ -280,70 +290,81 @@ int main (void)
             tinygl_update ();
             break;
         }
-               
-        
-        
+
+
+        // Starting the game
         while (start == 1) {
+            
             pacer_wait ();
             tinygl_update ();
-			if(game_state.player_selected == false){
-				select_player(&game_state);
-				continue;								
-			}else {
-			
-				if(game_state.selection_final == false){				
-					choose_selection(&game_state);
-				} else {
-					navswitch_update();
-					if(game_state.player == '1'){                    
-						get_opponent(&game_state);	
-						if(game_state.opponent != 0) {
-							if(game_state.result_set == false){
-								get_result(&game_state);
-							} else {
-								display_result(&game_state);
-							}					
-						}				
-					}	
-				
-				
-					if(game_state.player == '2'){
-						if(game_state.result_set == false){
-							ir_rcv_result(&game_state);					
-						} else {
-							invert_result(&game_state);
-							display_result(&game_state);
-						}			
-					}
-						
-						
-									
-					if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
-							reset_game(&game_state);
-			
-						}
-					
+            
+            // Check if the player is selected
+            if(game_state.player_selected == false) {
+                select_player(&game_state);
+                continue;
+            } else {
+                // Restart loop if the player hasent selected     
+                if(game_state.selection_final == false) {
+                    choose_selection(&game_state);
+                } else {
+                    navswitch_update();
+                    // Player one
+                    if(game_state.player == '1') {
+                        get_opponent(&game_state);
+                        // If the player 2 is available
+                        if(game_state.opponent != 0) {
+                            if(game_state.result_set == false) {
+                                // Check the results
+                                get_result(&game_state);
+                            } else {
+                                // Display the result
+                                display_result(&game_state);
+                            }
+                        }
+                    }
 
-					tick++;
-					if (tick > PACER_RATE / MESSAGE_RATE) {
-						tick = 0;
-						
-						
-						if(game_state.player == '1' && game_state.result_set){
-							snd_ir(&game_state, game_state.result);						
-						}
-						
-						if(game_state.player == '2'){
-							snd_ir(&game_state, game_state.selection);							
-						}
-						
+                    // Player two
+                    if(game_state.player == '2') {
+                        // Check if result is recived
+                        if(game_state.result_set == false) {
+                            // get the result
+                            ir_rcv_result(&game_state);
+                        } else {
+                            // Invert and display the results
+                            invert_result(&game_state);
+                            display_result(&game_state);
+                        }
+                    }
 
-					}
-				
 
-				}
-			}
-		
-}
-}
+                    // Reset game on navswitch push
+                    if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
+                        reset_game(&game_state);
+                    }
+
+                    // Sending IR at diffrent rates
+                    tick++;
+                    if (tick > PACER_RATE / MESSAGE_RATE) {
+                        tick = 0;
+
+                        // Check if result is set
+                        if(game_state.player == '1' && game_state.result_set) {
+                            // Send result to player 2
+                            snd_ir(&game_state, game_state.result);
+                        }
+
+                        if(game_state.player == '2') {
+                            // Send selection to player 1
+                            snd_ir(&game_state, game_state.selection);
+                        }
+
+
+                    }
+
+
+                }
+            }
+
+        }
+    }
 }
